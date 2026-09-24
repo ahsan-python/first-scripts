89f1929e3ac3774dbd48aebb4467ac11ac6c16ec
ahsan-python <ahsan.dev29@gmail.com>
2026-09-24 02:28:59 -0700

    Add files via upload

---
+++ Calculator.py
@@ -0,0 +1,6 @@
+print("==You opened a sum calculator==")
+num1 = int(input("Enter your first number:"))
+num2 = int(input("Enter your second number:"))
+
+result = num1 + num2
+print(f"The sum of {num1} and {num2} is {result}")
